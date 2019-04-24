fd6d51b8b17d080845d64074e307f72ca38a0800
Soren Thompson <soren.m.thompson.20@dartmouth.edu>
2019-04-23 22:15:02 -0400

    Workshop complete

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.360ed344d1208ee7bba1cb3f62cdf89f.js"
 );
 
 workbox.clientsClaim();
